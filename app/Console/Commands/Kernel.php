97aa9831b93a32cc5ea59e192846962111f28224
sandervanderkolk <sander@konstruktiv.nl>
2024-09-25 19:22:39 +0200

    update

--- app/Console/Commands/Kernel.php
+++ app/Console/Commands/Kernel.php
@@ -1,11 +1,13 @@
 <?php
 
+namespace App\Console\Commands;
+
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
         $schedule->command('command:daily-task')->dailyAt('06:00');
     }
