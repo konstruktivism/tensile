010f69611c7c4ddca9a32c74aba21e33d675223b
sandervanderkolk <sander@konstruktiv.nl>
2024-10-14 11:05:17 +0200

    Refactor scheduling for daily and weekly tasks

--- routes/console.php
+++ routes/console.php
@@ -1,14 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-
-Schedule::command('command:daily-task')->everyMinute();
+Schedule::command('command:daily-task')->dailyAt('06:00');
 
 Schedule::command('send:weekly-tasks')->fridays()->at('06:00')->environments(['production']);
 
