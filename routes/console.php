775958afaab4e252c1eb49ba0703ded14d4dd905
sandervanderkolk <sander@konstruktiv.nl>
2024-10-14 10:51:25 +0200

    Laravel 11 Scheduling commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('command:daily-task')->everyMinute();
+
+Schedule::command('send:weekly-tasks')->fridays()->at('06:00')->environments(['production']);
+
+Schedule::command('send:monthly-tasks')->monthlyOn(1, '06:00')->environments(['production']);
