b328dbc8b40b541775ef5b86365b1ea0436b0d50
sandervanderkolk <sander@konstruktiv.nl>
2024-10-28 14:04:29 +0100

    Add visual indicator for fixed-price projects in project view

--- routes/console.php
+++ routes/console.php
@@ -6,4 +6,4 @@ Schedule::command('command:daily-task')->dailyAt('06:00');
 
 Schedule::command('send:weekly-tasks')->fridays()->at('17:00')->environments(['production']);
 
-Schedule::command('send:monthly-tasks')->monthlyOn(1, '06:00')->environments(['production']);
+Schedule::command('send:monthly-tasks')->monthlyOn(1, '17:00')->environments(['production']);
