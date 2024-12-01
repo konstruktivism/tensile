789e44a47f2385dad58f43dd6d0420dff943cc31
sandervanderkolk <sander@konstruktiv.nl>
2024-12-01 16:24:53 +0100

    Modify time unit from hours to minutes in Moneybird export

--- app/Http/Controllers/MoneybirdController.php
+++ app/Http/Controllers/MoneybirdController.php
@@ -56,7 +56,7 @@ class MoneybirdController extends Controller
                     return [
                         'description' => $task->name,
                         'price' => $task->project->hour_tariff,
-                        'amount' => $task->minutes . ' minuten',
+                        'amount' => $task->minutes . ' minutes',
                         'ledger_account_id' => env('MONEYBIRD_LEDGER_ACCOUNT_ID'),
                     ];
                 })->toArray(),
