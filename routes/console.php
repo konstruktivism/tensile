0faaf747b8b4789a94a680bcc7faca1351de0e53
sandervanderkolk <sander@konstruktiv.nl>
2024-12-09 21:42:06 +0100

    sunday notification instead of friday

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('command:daily-task')->dailyAt('06:00');
 
-Schedule::command('send:weekly-tasks')->fridays()->at('23:00')->environments(['production']);
+Schedule::command('send:weekly-tasks')->sundays()->at('23:59')->environments(['production']);
 
 Schedule::command('send:monthly-tasks')->monthlyOn(1, '06:00')->when(function () {
     return now()->firstOfMonth()->isMonday();
