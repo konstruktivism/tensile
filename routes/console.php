d6c83d33b172c254f79c53f5b936db9492b74a50
sandervanderkolk <sander@konstruktiv.nl>
2025-07-14 11:43:00 +0200

    Add weekly import command to run before Sunday email notification

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('command:daily-task')->dailyAt('06:00');
 
+// Also run the import 5 minutes before the weekly email on Sundays
+Schedule::command('command:daily-task')->sundays()->at('23:54');
+
 Schedule::command('send:weekly-tasks')->sundays()->at('23:59')->environments(['production']);
 
 Schedule::command('send:monthly-tasks')->monthlyOn(1, '06:00')->when(function () {
