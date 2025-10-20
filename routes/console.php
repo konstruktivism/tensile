710c7e545b848d8cd137d2c5b46847ef6ba147f6
sandervanderkolk <sander@konstruktiv.nl>
2025-10-20 09:44:51 +0200

    Update terminal configuration in workspace for npm development and adjust scheduling commands in console.php to run weekly tasks on Fridays instead of Sundays.

--- routes/console.php
+++ routes/console.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('command:daily-task')->dailyAt('06:00');
 
-// Also run the import 5 minutes before the weekly email on Sundays
-Schedule::command('command:daily-task')->sundays()->at('23:54');
+// Also run the import 5 minutes before the weekly email on Fridays
+Schedule::command('command:daily-task')->fridays()->at('17:50');
 
-Schedule::command('send:weekly-tasks')->sundays()->at('23:59')->environments(['production']);
+Schedule::command('send:weekly-tasks')->fridays()->at('18:00')->environments(['production']);
 
 Schedule::command('send:monthly-tasks')->monthlyOn(1, '06:00')->when(function () {
     return now()->firstOfMonth()->isMonday();
