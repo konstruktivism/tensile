6cfe884770b8b203cfba6d188adbdde62805f947
sandervanderkolk <sander@konstruktiv.nl>
2026-01-30 13:20:33 +0100

    Change weekly tasks schedule from Friday evening to Saturday morning

--- routes/console.php
+++ routes/console.php
@@ -9,8 +9,8 @@ Schedule::command('command:daily-task')->dailyAt('06:00');
 Schedule::job(new JobCleanupForecastTasks, 'default', 'database')->hourlyAt(0);
 Schedule::job(new JobForecastImport, 'default', 'database')->hourlyAt(10);
 
-Schedule::command('import:weeks --weeks=1')->fridays()->at('20:30')->environments(['production']);
-Schedule::command('send:weekly-tasks')->fridays()->at('21:00')->environments(['production']);
+Schedule::command('import:weeks --weeks=1')->saturdays()->at('08:30')->environments(['production']);
+Schedule::command('send:weekly-tasks')->saturdays()->at('09:00')->environments(['production']);
 
 Schedule::command('import:monthly')->monthlyOn(1, '05:30')->when(function () {
     return now()->firstOfMonth()->isMonday();
